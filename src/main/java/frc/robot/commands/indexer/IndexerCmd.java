a32739b2e27d30c74883d3b6b590dfca67f5299c
AndrewH234567 <86069642+AndrewH234567@users.noreply.github.com>
2026-01-31 11:36:53 -0500

    Indexer command

--- src/main/java/frc/robot/commands/indexer/IndexerCmd.java
+++ src/main/java/frc/robot/commands/indexer/IndexerCmd.java
@@ -1,9 +1,41 @@
 package frc.robot.commands.indexer;
 
+import java.util.Queue;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.indexer.IndexerSubsystem;
-import frc.robot.subsystems.intake.IntakeSubsystem;
 
 public class IndexerCmd extends Command {
+  private final IndexerSubsystem m_Indexer;
+  private final double m_speed;
 
+  public IndexerCmd(IndexerSubsystem subsystem, double speed) {
+    m_Indexer = subsystem;
+    m_speed = speed;
+
+    addRequirements(m_Indexer);
+  }
+
+  @Override
+  public void initialize() {
+    System.out.println("Indexer CMD Started");
+    m_Indexer.setVelocity(m_speed);
+  }
+
+  @Override
+  public void execute() {
+
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("Intake CMD ended.");
+
+    m_Indexer.setVelocity(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- src/main/java/frc/robot/commands/launcher/Launcher.java
+++ src/main/java/frc/robot/commands/launcher/Launcher.java
@@ -1,8 +1,9 @@
 package frc.robot.commands.launcher;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.launcher.*;
 
 public class Launcher extends Command {
+  private final LauncherSubsystem laucher;
 
 }
