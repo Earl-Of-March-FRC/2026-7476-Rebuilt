3b66857ae36f64bb348dd98d36d48eb4516c7769
Sadra-Hemmati <sadra.hemmati@gmail.com>
2026-02-28 01:19:22 -0500

    WIP: Use a mix of subclasses and inline commands for cleaner composition

---
+++ src/main/java/frc/robot/commands/groups/LaunchAndIndexCmd.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.groups;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.IndexerConstants;
+import frc.robot.commands.indexer.IndexerCmd;
+import frc.robot.commands.launcherAndIntake.LauncherCmd;
+import frc.robot.subsystems.indexer.IndexerSubsystem;
+import frc.robot.subsystems.launcherAndIntake.LauncherAndIntakeSubsystem;
+import frc.robot.util.PoseHelpers;
+import frc.robot.util.launcher.LaunchHelpers;
+
+public class LaunchAndIndexCmd extends ParallelCommandGroup {
+  /**
+   * Creates a new LaunchAndIndexCmd.
+   * 
+   * @param indexerSub           The IndexerSubsystem to use
+   * @param launcherAndIntakeSub The LauncherAndIntakeSubsystem to use
+   * @param launchSupplier       A BooleanSupplier that determines whether to
+   *                             laucnh balls or not (controls the indexer wheel)
+   */
+  public LaunchAndIndexCmd(IndexerSubsystem indexerSub, LauncherAndIntakeSubsystem launcherAndIntakeSub,
+      BooleanSupplier launchSupplier) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(new IndexerCmd(
+        indexerSub,
+        () -> launchSupplier.getAsBoolean()
+            ? IndexerConstants.kWheelLaunchIndexPercent
+            : 0,
+        () -> IndexerConstants.kTreadmillLaunchIndexPercent),
+        new LauncherCmd(launcherAndIntakeSub,
+            () -> LaunchHelpers.calculateWheelRPM()));
+  }
+
+  /**
+   * Convenience constructor for auto launching when we think the ball will go in
+   * 
+   * @param indexerSub           The IndexerSubsystem to use
+   * @param launcherAndIntakeSub The LauncherAndIntakeSubsystem to use
+   */
+  public LaunchAndIndexCmd(IndexerSubsystem indexerSub, LauncherAndIntakeSubsystem launcherAndIntakeSub) {
+    this(
+        indexerSub,
+        launcherAndIntakeSub,
+        () -> LaunchHelpers.willHitTarget(
+            PoseHelpers.getAllianceHubtTranslation3d(),
+            FieldConstants.kHubInsideWidth));
+  }
+
+}
