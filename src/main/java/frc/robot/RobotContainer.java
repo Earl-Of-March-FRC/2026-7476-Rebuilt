6d56af2a270fae2a35bc3666840453aadca3ead1
IanGibson84 <igibs1@ocdsb.ca>
2026-03-07 11:54:12 -0500

    add a binding for intaking with launcher

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -302,6 +302,8 @@ public class RobotContainer {
     driverController.rightBumper().toggleOnTrue(new IndexerCmd(indexerSub, () -> IndexerConstants.kWheelSpeed,
         () -> IndexerConstants.kTreadmillSpeed));
 
+    driverController.rightBumper().toggleOnTrue(new LauncherCmd(launcherSub, () -> RPM.of(400)));
+
     driverController.leftBumper().toggleOnTrue(new IndexerCmd(indexerSub, () -> -IndexerConstants.kWheelSpeed,
         () -> -IndexerConstants.kTreadmillSpeed));
 
