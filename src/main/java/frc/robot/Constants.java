29c0a124325e081e9a96264eb8e066927c3c1139
Bruce Zhang <bqzh2019@gmail.com>
2026-04-11 15:23:11 -0400

    Improve shooting rpm multiplier to 1.1x

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -204,7 +204,7 @@ public final class Constants {
     // For fine tuning due to small changes
     // TODO replace with final value after testing
     private static final LoggedNetworkNumber kRPMCurveMultiplier = new LoggedNetworkNumber("/Tuning/RPMCurveMultiplier",
-        1.0);
+        1.1);
     public static final Function<Distance, AngularVelocity> kDistanceToRPMCurve = (Distance distance) -> {
       double d = distance.in(Meters);
       double rpm = kRPMCurveA * d * d + kRPMCurveB * d + kRPMCurveC;
